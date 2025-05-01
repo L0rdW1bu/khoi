35b1ac1be3a853813161bce09e09b6ca98997889
Coinba <khoi.nguyendangba@gmail.com>
2025-05-01 22:37:24 +0700

    them playerhealth

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
@@ -8,6 +10,8 @@ public class PlayerStats : MonoBehaviour
     public float health;
     public float maxHealth;
     public GameObject player;
+    public TextMeshProUGUI healthText;
+    public Slider healthSlider;
     void Awake()
     {
         if(playerStats !=null)
@@ -24,17 +28,27 @@ public class PlayerStats : MonoBehaviour
     void Start()
     {
         health = maxHealth;
+        healthSlider.value = 1;
+        SetHealthUI();
     }
     public void DealDamage(float damage)
     {
         health -= damage;
         CheckDeath();
+        SetHealthUI();
     }
     public void HealCharacter(float heal)
     {
         health += heal;
         CheckOverHealth();
+        SetHealthUI();
     }
+    private void SetHealthUI()
+    {
+        healthSlider.value = CaculateHealthPercentage();
+        healthText.text = Mathf.Ceil(health).ToString() + " / " + Mathf.Ceil(maxHealth).ToString();
+    }
+       
     private void CheckOverHealth()
     {
         if (health > maxHealth)
@@ -46,7 +60,12 @@ public class PlayerStats : MonoBehaviour
     {
         if (health <= 0)
         {
+            health = 0;
             Destroy(player);
         }
+    }
+    float CaculateHealthPercentage()
+    {
+        return health / maxHealth;
     }
 }
--- Assets/Scripts/TestEnemyShooting2.cs
+++ Assets/Scripts/TestEnemyShooting2.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class TestEnemyShooting2 : MonoBehaviour
 {
     public GameObject projectile2;
-    public Transform player2;
+    public GameObject player2;
     public float minDamage2;
     public float maxDamage2;
     public float projectileForce2;
@@ -14,6 +14,7 @@ public class TestEnemyShooting2 : MonoBehaviour
     void Start()
     {
         StartCoroutine(ShootPlayer2());
+        player2 = FindObjectOfType<PlayerMovement>().gameObject;
     }
 
     IEnumerator ShootPlayer2()
@@ -23,13 +24,13 @@ public class TestEnemyShooting2 : MonoBehaviour
         {
             GameObject spell = Instantiate(projectile2, transform.position, Quaternion.identity);
             Vector2 myPos = transform.position;
-            Vector2 targetPos = player2.position;
+            Vector2 targetPos = player2.transform.position;
             Vector2 direction = (targetPos - myPos).normalized;
 
             
             var projectileScript = spell.GetComponent<TestEnemyProjectiles2>();
             projectileScript.damage = Random.Range(minDamage2, maxDamage2);
-            projectileScript.target = player2; 
+            projectileScript.target = player2.transform; 
             projectileScript.moveSpeed = projectileForce2;
 
             StartCoroutine(ShootPlayer2());
